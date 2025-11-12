bfbfe9e3a37d3a311f1d1e6f045738f7e1267db1
Johnnydev <ruthspenser85@gmail.com>
2025-11-12 12:32:56 +0000

    index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "johnny-tagger";
+import { componentTagger } from "johnny-tagger"; // import tagger from "johnny
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
