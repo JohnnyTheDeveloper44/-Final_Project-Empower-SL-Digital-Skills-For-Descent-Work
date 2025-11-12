d5bc4e2c696787434961243f6ce6a031fadca1a4
Johnnydev <ruthspenser85@gmail.com>
2025-11-12 01:56:10 +0000

    indext.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",  // <- Add this line for proper Netlify deployment
   server: {
     host: "::",
     port: 8080,
